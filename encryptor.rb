cf03efb105b8d9a5f0ea5f1efc9e49098e7877bf
Shaun McClain <mcclains2003@gmail.com>
2015-08-05 15:40:42 -0500

    Functionality that now allows encrypt to accept strings

--- encryptor.rb
+++ encryptor.rb
@@ -10,7 +10,20 @@ class Encryptor
      'y' => 'l', 'z' => 'm'}
   end
 
-  def encrypt(letter)
+  def encrypt(string)
+    # 1. Cut the input string into letters
+    letters = string.split("")
+    # 2. Encrypt those letters one at a time, gathering the results
+    results = []
+    letters.each do |letter|
+      encrypted_letter = encrypt_letter(letter)
+      results.push(encrypted_letter)
+    end
+    # 3. Join the results back together in one string
+    results.join
+  end
+
+  def encrypt_letter(letter)
     lowercase_letter = letter.downcase
     cipher[lowercase_letter]
   end
