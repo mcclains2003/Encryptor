cb8a2bba696740e04139bd9e4235fd2848a6c56e
Shaun McClain <mcclains2003@gmail.com>
2015-08-06 14:47:16 -0500

    Updating after deletion of files. Starting over. Basic cipher and spec file added

---
+++ encryptor.rb
@@ -0,0 +1,24 @@
+class Encryptor
+
+  def cipher(rotation)
+    characters = (' '..'z').to_a
+    rotatated_characters = characters.rotate(rotation)
+    Hash[characters.zip(rotatated_characters)]
+  end
+
+  def encrypt(string,rotation)
+    letters = string.split("")
+
+    results = letters.collect do |letter|
+      encrypted_letter = encrypt_letter(letter,rotation)
+    end
+
+    results.join
+  end
+
+  def encrypt_letter(letter,rotation)
+    cipher_for_rotation = cipher(rotation)
+    cipher_for_rotation[letter]
+  end
+
+end
